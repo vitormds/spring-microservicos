7d5c2f65604775b1fb28acd2d1b079b3b68b5ecc
Resultados agora <dev_vitorsouza@hotmail.com>
2021-01-24 16:09:13 -0300

    project payroll created

---
+++ hr-payroll/src/main/java/com/hr/payroll/HrPayrollApplication.java
@@ -0,0 +1,13 @@
+package com.hr.payroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrPayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrPayrollApplication.class, args);
+	}
+
+}
