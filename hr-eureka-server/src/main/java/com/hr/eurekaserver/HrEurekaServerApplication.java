67440706038041645bac268825e9dacf5b7fa8d3
Resultados agora <dev_vitorsouza@hotmail.com>
2021-01-25 23:21:08 -0300

    Configure Eureka Server

--- hr-eureka-server/src/main/java/com/hr/eurekaserver/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/com/hr/eurekaserver/HrEurekaServerApplication.java
@@ -2,10 +2,10 @@ package com.hr.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
 @SpringBootApplication
 public class HrEurekaServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HrEurekaServerApplication.class, args);
 	}
