773b7ffa5daff350ad95367295efa5e4b17e420c
Resultados agora <dev_vitorsouza@hotmail.com>
2021-02-14 02:10:41 -0300

    How to check if the properties are passing the environment variables

--- hr-config-server/src/main/java/com/hr/configserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/hr/configserver/HrConfigServerApplication.java
@@ -1,14 +1,27 @@
 package com.hr.configserver;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @SpringBootApplication
-public class HrConfigServerApplication {
+public class HrConfigServerApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
+	}
+	@Value("${spring.cloud.config.server.git.username}")
+	private String username;
+	@Value("${spring.cloud.config.server.git.password}")
+	private String password;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("USERNAME = "+ username);
+		System.out.println("PASSWORD= "+ password);
+		
 	}
 
 }
