a4c1c1b6c44dbba23dbe892dfa2daf5b1cee5ae6
Resultados agora <dev_vitorsouza@hotmail.com>
2021-02-17 22:07:21 -0300

    Login e token JWT PARTE 1

---
+++ hr-oauth/src/main/java/com/hr/oauth/config/AppConfig.java
@@ -0,0 +1,27 @@
+package com.hr.oauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+public class AppConfig {
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+		accessTokenConverter.setSigningKey("MY-SECRET-KEY");
+		return accessTokenConverter;
+	}
+	@Bean
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
+	
+}
