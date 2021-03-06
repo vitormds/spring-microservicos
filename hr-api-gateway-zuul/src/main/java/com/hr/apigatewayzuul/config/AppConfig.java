5af8bd74d167608319a610f0311b8891474f5a27
Resultados agora <dev_vitorsouza@hotmail.com>
2021-03-06 00:01:35 -0300

    Autorização pelo Zuul

---
+++ hr-api-gateway-zuul/src/main/java/com/hr/apigatewayzuul/config/AppConfig.java
@@ -0,0 +1,21 @@
+package com.hr.apigatewayzuul.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+@Configuration
+public class AppConfig {
+
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+		accessTokenConverter.setSigningKey("MY-SECRET-KEY");
+		return accessTokenConverter;
+	}
+	@Bean
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
+}
